bdf9517a7d7aa01ec0f41bbd26c924febaef07d8
Azusa Atsumi <azusamamu@yahoo.co.jp>
2020-01-27 04:47:34 +0000

    　correction

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function(){
-  Route::get('news/create','Admin\NewsController@add');
-});
 
 //課題3
-Route::get('XXX','AAAController@bbb');
+//Route::get('XXX','AAAController@bbb');
 
-//課題４
-Route:group(['prefix' => 'admin'], function(){
+//課題4
+Route::group(['prefix' => 'admin'], function(){
+  Route::get('news/create','Admin\NewsController@add');
   Route::get('profile/create','Admin\ProfileController@add');
   Route::get('profile/edit','Admin\ProfileController@edit');
 });
